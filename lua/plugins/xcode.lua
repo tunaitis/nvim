d13c32db1ec4fec99bf75482ed3dfe4916e32598
simtun <simonas.tunaitis@gmail.com>
2024-06-18 16:37:24 +0300

    Configure Swift LSP

--- lua/plugins/xcode.lua
+++ lua/plugins/xcode.lua
@@ -4,11 +4,22 @@ return {
     dependencies = {
       "nvim-telescope/telescope.nvim",
       "MunifTanjim/nui.nvim",
+      "neovim/nvim-lspconfig",
     },
     config = function()
       require("xcodebuild").setup({
         -- put some options here or leave it empty to use default settings
       })
     end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    event = "LazyFile",
+    opts = {
+      inlay_hints = { enabled = false },
+      servers = {
+        sourcekit = {},
+      },
+    },
   },
 }
