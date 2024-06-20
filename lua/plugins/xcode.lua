7554f8fa5cc222aa19642f33ccd3664c3f429b09
simtun <simonas.tunaitis@gmail.com>
2024-06-20 22:21:13 +0300

    Add Xcode keymaps and debugging support

--- lua/plugins/xcode.lua
+++ lua/plugins/xcode.lua
@@ -18,7 +18,12 @@ return {
     opts = {
       inlay_hints = { enabled = false },
       servers = {
-        sourcekit = {},
+        sourcekit = {
+          keys = {
+            { "<leader>xr", "<cmd>XcodebuildBuildRun<CR>", desc = "Xcode Run" },
+            { "<leader>xd", "<cmd>XcodeDebug<CR>", desc = "Xcode Debug" },
+          },
+        },
       },
     },
   },
@@ -53,5 +58,35 @@ return {
         swift = { "swiftformat" },
       },
     },
+  },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "wojciech-kulik/xcodebuild.nvim",
+    },
+    config = function()
+      local xcodebuild = require("xcodebuild.integrations.dap")
+      -- SAMPLE PATH, change it to your local codelldb path
+      local codelldbPath = os.getenv("HOME") .. "/tools/codelldb/adapter/codelldb"
+
+      xcodebuild.setup(codelldbPath)
+
+      vim.api.nvim_create_user_command("XcodeDebug", function()
+        require("dap").disconnect()
+        -- require("dapui").close()
+        require("xcodebuild.integrations.dap").build_and_debug()
+      end, {})
+
+      --lvim.keys.normal_mode["xr"] = {":XcodebuildBuildRun <cr>", {desc = "Xcode Build & Debug" }}
+
+      --vim.keymap.set("n", "xr", xcodebuild.build_and_debug, { desc = "Xcode Build & Debug" })
+      --vim.keymap.set("n", "<leader>dd", xcodebuild.build_and_debug, { desc = "Build & Debug" })
+      --vim.keymap.set("n", "<leader>dr", xcodebuild.debug_without_build, { desc = "Debug Without Building" })
+      --vim.keymap.set("n", "<leader>dt", xcodebuild.debug_tests, { desc = "Debug Tests" })
+      --vim.keymap.set("n", "<leader>dT", xcodebuild.debug_class_tests, { desc = "Debug Class Tests" })
+      --vim.keymap.set("n", "<leader>b", xcodebuild.toggle_breakpoint, { desc = "Toggle Breakpoint" })
+      --vim.keymap.set("n", "<leader>B", xcodebuild.toggle_message_breakpoint, { desc = "Toggle Message Breakpoint" })
+      --vim.keymap.set("n", "<leader>dx", xcodebuild.terminate_session, { desc = "Terminate Debugger" })
+    end,
   },
 }
